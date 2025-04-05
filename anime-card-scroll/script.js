b77f5d00ce8ca7821f38040b4c20a26966019914
AlphaCodeIn <luckychauhan.12W@gmail.com>
2025-04-05 16:02:53 +0530

    Added anime card scroll effect

--- anime-card-scroll/script.js
+++ anime-card-scroll/script.js
@@ -1,69 +1,77 @@
 const characters = [
-  {
-    name: "Naruto Uzumaki",
-    image: "./img/naruto.png",
-    bgColor: "#E7C241",
-    age: 17,
-    rank: "Hokage",
-    clan: "Uzumaki",
-    powerLevel: 95,
-  },
-  {
-    name: "Sakura Haruno",
-    image: "./img/Sakura.png",
-    bgColor: "#ea8e89",
-    age: 17,
-    rank: "Jonin",
-    clan: "Haruno",
-    powerLevel: 78,
-  },
-  {
-    name: "Sasuke Uchiha",
-    image: "./img/sasuke.png",
-    bgColor: "#bc5ba7",
-    age: 17,
-    rank: "Rogue Shinobi",
-    clan: "Uchiha",
-    powerLevel: 94,
-  },
-  {
-    name: "Kakashi Hatake",
-    image: "./img/kakasi.png",
-    bgColor: "#908C8A",
-    age: 29,
-    rank: "Former Hokage",
-    clan: "Hatake",
-    powerLevel: 89,
-  },
-  {
-    name: "Jiraiya",
-    image: "./img/jiraya.png",
-    bgColor: "#dbb681",
-    age: 54,
-    rank: "Legendary Sannin",
-    clan: "None",
-    powerLevel: 88,
-  },
-  {
-    name: "Tobi (Obito)",
-    image: "./img/tobi.png",
-    bgColor: "#d05b13",
-    age: 31,
-    rank: "Akatsuki Leader",
-    clan: "Uchiha",
-    powerLevel: 93,
-  },
-  {
-    name: "Hokage (Minato)",
-    image: "./img/hokage.png",
-    bgColor: "#a16145",
-    age: 24,
-    rank: "4th Hokage",
-    clan: "Namikaze",
-    powerLevel: 91,
-  },
-];
-
+    {
+      name: "Naruto Uzumaki",
+      image: "./img/naruto.png",
+      bgLight: "#ffea81",
+      bgDark: "#a87c00",
+      age: 17,
+      rank: "Hokage",
+      clan: "Uzumaki",
+      powerLevel: 95,
+    },
+    {
+      name: "Sakura Haruno",
+      image: "./img/Sakura.png",
+      bgLight: "#f9b6b2",
+      bgDark: "#e6726b",
+      age: 17,
+      rank: "Jonin",
+      clan: "Haruno",
+      powerLevel: 78,
+    },
+    {
+      name: "Sasuke Uchiha",
+      image: "./img/sasuke.png",
+      bgLight: "#e6a8dc",
+      bgDark: "#a94491",
+      age: 17,
+      rank: "Rogue Shinobi",
+      clan: "Uchiha",
+      powerLevel: 94,
+    },
+    {
+      name: "Kakashi Hatake",
+      image: "./img/kakasi.png",
+      bgLight: "#b0adac",
+      bgDark: "#5c5b5a",
+      age: 29,
+      rank: "Former Hokage",
+      clan: "Hatake",
+      powerLevel: 89,
+    },
+    {
+      name: "Jiraiya",
+      image: "./img/jiraya.png",
+      bgLight: "#f3d9ae",
+      bgDark: "#c58c39",
+      age: 54,
+      rank: "Legendary Sannin",
+      clan: "None",
+      powerLevel: 88,
+    },
+    {
+      name: "Tobi (Obito)",
+      image: "./img/tobi.png",
+      bgLight: "#ff9354",
+      bgDark: "#b34808",
+      age: 31,
+      rank: "Akatsuki Leader",
+      clan: "Uchiha",
+      powerLevel: 93,
+    },
+    {
+      name: "Hokage (Minato)",
+      image: "./img/hokage.png",
+      bgLight: "#d78c5f",
+      bgDark: "#844f2d",
+      age: 24,
+      rank: "4th Hokage",
+      clan: "Namikaze",
+      powerLevel: 91,
+    },
+  ];
+  
+  
 //   -------
 const main = document.querySelector(".card-wrap");
 const cardWidth = 320 + 120;
@@ -79,18 +87,32 @@ document.getElementById("prev").addEventListener("click", () => {
 let allCards = document.querySelector(".card-wrap");
 
 characters.forEach(function (char) {
-  allCards.innerHTML += `
-    <div class="card" style="background-color:${char.bgColor}; box-shadow: 0 0 50px ${char.bgColor}66;">
-            <img src="${char.image}" alt="">
-            <div class="details">
-            <h2>${char.name}, ${char.age}</h2>
-            <p>Rank: ${char.rank}</p>
-            <p>Clan: ${char.clan}</p>
-            <p>Power Level: ${char.powerLevel}</p>
-</div>
-      
+ allCards.innerHTML += `
+  <div class="card" style="background: linear-gradient(to top right, ${char.bgDark}, ${char.bgLight}); box-shadow: 0 0 50px ${char.bgLight}66;">
+    <img src="${char.image}" alt="">
+    <div class="details">
+      <h2>${char.name}, ${char.age}</h2>
+      <p>Rank: ${char.rank}</p>
+      <p>Clan: ${char.clan}</p>
+      <p>Power Level: ${char.powerLevel}</p>
+    </div>
+  </div>
+`;
 
-
-        </div>
-    `;
 });
+
+    let cur = document.querySelector("#cursore");
+
+    document.querySelector("body").addEventListener("mousemove", function (dets) {
+        cur.style.left = `${dets.clientX - 10}px`;
+    cur.style.top = `${dets.clientY - 10}px`;
+    });
+
+    document.body.addEventListener("mousedown", () => {
+  cur.style.transform = "scale(0.6)";
+});
+
+document.body.addEventListener("mouseup", () => {
+  cur.style.transform = "scale(1)";
+});
+    
